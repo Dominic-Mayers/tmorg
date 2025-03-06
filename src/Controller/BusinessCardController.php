7543f18239daf010113842e266bd1a7b689f4964
Dominic Mayers <dominic.mayers@meditationstudies.org>
2025-03-06 08:18:47 -0500

    Temporarily Out of Service

--- src/Controller/BusinessCardController.php
+++ src/Controller/BusinessCardController.php
@@ -14,8 +14,8 @@ class BusinessCardController
         \ob_start(); 
         require("../governors/business_card.php"); 
         $resp = \ob_get_clean(); 
-        // return new Response($resp); 
-        return new Response("<html><body>Temporary Out of Service.</body></html>"); 
+        //return new Response($resp); 
+        return new Response("<html><body>Temporarily Out of Service.</body></html>"); 
     }
 
     //[Route('/businesscardtopdf')]
